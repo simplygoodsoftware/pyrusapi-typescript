2f5c870cdfef2b39aad074dc7c35093abd9001cb
Alexander Dmitriev <admitriev@pyrus.com>
2026-03-02 14:49:47 +0300

    fix type

--- src/entities/formFields/formFieldCatalog.ts
+++ src/entities/formFields/formFieldCatalog.ts
@@ -3,11 +3,11 @@ import {FormFieldType} from "../../enums/formFieldType";
 
 export type FormFieldCatalog = FormFieldBase & {
     type?: typeof FormFieldType.Catalog;
-    value?: Catalog;
+    value?: CatalogValue;
 };
 
-type Catalog = {
-    item_id: number;
+type CatalogValue = {
+    item_id?: number;
     item_ids?: number[];
     item_names?: string[];
     headers?: string[];
