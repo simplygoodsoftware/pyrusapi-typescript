f94fda1e194bde1aa5bb21ce55d0dd2b3309cbdb
vgusak-g <vgusak@pyrus.com>
2026-05-27 14:11:23 +0300

    add enum channelDirection

--- src/entities/channel.ts
+++ src/entities/channel.ts
@@ -1,10 +1,11 @@
 import {ChannelType} from "../enums/channelType";
+import {ChannelDirection} from "../enums/channelDirection";
 import {ChannelUser} from "./channelUser";
 
 export type Channel = {
     type: ChannelType;
     phone?: string;
-    direction?: string;
+    direction?: ChannelDirection;
     to?: ChannelUser;
     from?: ChannelUser;
 };
