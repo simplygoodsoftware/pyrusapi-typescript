1c9d4b3681e90837cb12d59f10c7d128a3778419
iilyushenok <53813385+iilyushenok@users.noreply.github.com>
2026-06-19 15:17:49 +0300

    Take API URLs from environment

--- src/settings/defaults.ts
+++ src/settings/defaults.ts
@@ -1,7 +1,22 @@
 import {Settings} from "../entities/settings";
 
+const isNode: boolean =
+    typeof process !== "undefined" &&
+    process.versions != null &&
+    process.versions.node != null;
+const env = isNode ? process.env : {};
+const authUrlFromEnvironment = env["PYRUS_API_AUTH_URL"];
+const apiUrlFromEnvironment = env["PYRUS_API_URL"];
+const filesUrlFromEnvironment = env["PYRUS_API_FILES_URL"];
+
 export const defaults: Settings = {
-    authUrl: "https://accounts.pyrus.com/api/v4",
-    apiUrl: "https://api.pyrus.com/v4",
-    filesUrl: "https://files.pyrus.com",
+    authUrl: !!authUrlFromEnvironment
+        ? authUrlFromEnvironment
+        : "https://accounts.pyrus.com/api/v4",
+    apiUrl: !!apiUrlFromEnvironment
+        ? apiUrlFromEnvironment
+        : "https://api.pyrus.com/v4",
+    filesUrl: !!filesUrlFromEnvironment
+        ? filesUrlFromEnvironment
+        : "https://files.pyrus.com",
 };
